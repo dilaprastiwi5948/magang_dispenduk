a5f386708d5a30676dd5c218b56de038d1c2c308
chikaLabita <itaaa.c2@gmail.com>
2022-11-08 14:25:35 +0700

    Update : Login Form

--- KTP/resources/views/auth/login.blade.php
+++ KTP/resources/views/auth/login.blade.php
@@ -1,60 +1,54 @@
-@extends('template.index')
+<!DOCTYPE html>
+<html>
 
-@push('style')
-    <style>
-        /* body {
-            background: none;
-        } */
+<head>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet"
+        crossorigin="anonymous">
+    <title>Login</title>
+</head>
 
-        #wrapper {
-            display: flex;
-            flex-direction: column;
-            justify-content: center;
-            align-items: center;
-            min-height: 100vh;
-        }
-
-    </style>
-@endpush
-
-@section('body')
-
-<div class="container">
-    <div class="panel panel-default">
-        <div class="panel-heading">
-            <div class="card-title">
-                <div class="title">Login page</div>
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-10 col-md-5 col-lg-5 mx-auto">
+                <div class="card border-0 shadow rounded-3 my-5">
+                    <div class="card-body p-4 p-sm-5">
+                        <h5 class="card-title text-center mb-5 fw-dark fs-5">
+                            <img src="https://turbo.net.id/wp-content/webp-express/webp-images/uploads/2020/12/19196952-removebg-preview.png.webp"
+                                class="rounded-circle" style="width: 150px;" alt="Avatar" /><br>
+                            SIGN IN
+                        </h5>
+                        <form action="{{ route('login.submit') }}" method="POST">
+                            @csrf
+                            <div class="col-md-12 mb-2">
+                                <label>Username</label>
+                                <input type="text" class="form-control" id="inputUsername" name="username"
+                                    placeholder="Username" autofocus>
+                                @error('username')
+                                <small id="usernameid" class="text-danger">{{$message}}</small>
+                                @enderror
+                            </div>
+                            <div class="col-md-12 mb-2">
+                                <label>Password</label>
+                                <input type="password" class="form-control" id="inputPassword" name="password"
+                                    placeholder="Password">
+                                @error('password')
+                                <small id="passwordid" class="text-danger">{{$message}}</small>
+                                @enderror
+                            </div>
+                            <div class="d-grid">
+                                <button class="btn btn-primary btn-login text-uppercase fw-bold" type="submit">Sign
+                                    in</button>
+                                <p><br>Don't have an account?
+                                    <a class="link-danger" href="{{ route('register') }}">Register</a>
+                                </p>
+                            </div>
+                        </form>
+                    </div>
+                </div>
             </div>
         </div>
-        <div class="panel-body">
-            <form class="form-horizontal" action="{{ route('login.submit') }}" method="POST">
-                @csrf
-                <div class="form-group @error('username') has-error @enderror">
-                    <label for="inputEmail3" class="col-sm-2 control-label">Username</label>
-                    <div class="col-sm-10">
-                        <input type="text" class="form-control" id="inputUsername" placeholder="Username" name="username" value="{{old('username')}}">
-                        @error('username')
-                        <small id="usernameid" class="text-danger">{{$message}}</small>
-                        @enderror
-                    </div>
-                </div>
-                <div class="form-group @error('password') has-error @enderror">
-                    <label for="inputEmail3" class="col-sm-2 control-label">Password</label>
-                    <div class="col-sm-10">
-                        <input type="password" class="form-control" id="inputPassword" placeholder="password" name="password"">
-                        @error('password')
-                        <small id="passwordid" class="text-danger">{{$message}}</small>
-                        @enderror
-                    </div>
-                </div>
-                <div class="form-group">
-                    <div class="col-sm-offset-2 col-sm-10">
-                        <button type="submit" class="btn btn-primary">Sign in</button>
-                        <a href="{{ route('register') }}" class="btn btn-default" role="button">Register</a>
-                    </div>
-                </div>
-            </form>
-        </div>
     </div>
-</div>
-@endsection
+</body>
+
+</html>
